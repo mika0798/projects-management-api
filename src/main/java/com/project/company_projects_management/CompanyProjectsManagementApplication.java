c384e5dc700964a494177bdf7a9bea9e0ba6f213
mkn <192448446+mika0798@users.noreply.github.com>
2025-12-08 20:37:58 +0700

    Initial commit

---
+++ src/main/java/com/project/company_projects_management/CompanyProjectsManagementApplication.java
@@ -0,0 +1,13 @@
+package com.project.company_projects_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompanyProjectsManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompanyProjectsManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/company_projects_management/CompanyProjectsManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.company_projects_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompanyProjectsManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
