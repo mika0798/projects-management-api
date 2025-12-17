e9851b3f5dae59b4e5c8c1b1297eedcaa4d60ab1
mkn <192448446+mika0798@users.noreply.github.com>
2025-12-17 16:35:13 +0700

    All methods for DAO

--- src/main/java/com/project/company_projects_management/dao/EmployeeDAO.java
+++ src/main/java/com/project/company_projects_management/dao/EmployeeDAO.java
@@ -6,4 +6,7 @@ import java.util.List;
 
 public interface EmployeeDAO {
     List<Employee> findAll();
+    Employee findById(Long id);
+    Employee save(Employee employee);
+    void deleteById(Long id);
 }
