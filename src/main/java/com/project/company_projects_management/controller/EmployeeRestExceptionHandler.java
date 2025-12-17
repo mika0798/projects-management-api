3fb9607c0c3844c924f784c145972307858e6541
mkn <192448446+mika0798@users.noreply.github.com>
2025-12-18 00:28:02 +0700

    Add exception handler

---
+++ src/main/java/com/project/company_projects_management/controller/EmployeeRestExceptionHandler.java
@@ -0,0 +1,35 @@
+package com.project.company_projects_management.controller;
+
+import com.project.company_projects_management.dto.EmployeeErrorResponse;
+import com.project.company_projects_management.exception.EmployeeNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class EmployeeRestExceptionHandler {
+
+    @ExceptionHandler(EmployeeNotFoundException.class)
+    public ResponseEntity<EmployeeErrorResponse> handleEmployeeNotFoundException(EmployeeNotFoundException e) {
+        EmployeeErrorResponse employeeErrorResponse = new EmployeeErrorResponse();
+
+        employeeErrorResponse.setStatus(HttpStatus.NOT_FOUND.value());
+        employeeErrorResponse.setMessage(e.getMessage());
+        employeeErrorResponse.setTimeStamp(System.currentTimeMillis());
+
+        return new ResponseEntity<>(employeeErrorResponse, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<EmployeeErrorResponse> handleException(Exception e) {
+        EmployeeErrorResponse employeeErrorResponse = new EmployeeErrorResponse();
+
+        employeeErrorResponse.setStatus(HttpStatus.BAD_REQUEST.value());
+        employeeErrorResponse.setMessage(e.getMessage());
+        employeeErrorResponse.setTimeStamp(System.currentTimeMillis());
+
+        return new ResponseEntity<>(employeeErrorResponse, HttpStatus.BAD_REQUEST);
+    }
+
+}
