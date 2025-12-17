1d6311545ad3254b03b1c84a738419778e480024
mkn <192448446+mika0798@users.noreply.github.com>
2025-12-17 16:36:48 +0700

    -GET, GET with id endpoints -Swagger documenting

--- src/main/java/com/project/company_projects_management/controller/EmployeeController.java
+++ src/main/java/com/project/company_projects_management/controller/EmployeeController.java
@@ -3,11 +3,14 @@ package com.project.company_projects_management.controller;
 import com.project.company_projects_management.entity.Employee;
 import com.project.company_projects_management.service.EmployeeService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.constraints.Min;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -23,9 +26,17 @@ public class EmployeeController {
         this.employeeService = employeeService;
     }
 
-    @Operation(summary = "Get all employees",description = "Retrieve list of all employees0")
     @GetMapping
+    @Operation(summary = "Get all employees",description = "Retrieve list of all employees")
     public ResponseEntity<List<Employee>> getAllEmployees() {
         return ResponseEntity.ok(employeeService.getAllEmployees());
+    }
+
+    @GetMapping("/{id}")
+    @Operation(summary="Get an employee by Id", description = "Retrieve an employee by their Id")
+    public ResponseEntity<Employee> getEmployeeById(
+                            @Parameter(description = "Id of employee to be retrieved")
+                            @PathVariable @Min(value = 1) Long id) {
+        return ResponseEntity.ok(employeeService.getEmployeeById(id));
     }
 }
