a0970502afae73aae50b3ec388b9ca03147b0201
mkn <192448446+mika0798@users.noreply.github.com>
2025-12-17 16:35:43 +0700

    All methods for Service Impl

--- src/main/java/com/project/company_projects_management/service/EmployeeServiceImpl.java
+++ src/main/java/com/project/company_projects_management/service/EmployeeServiceImpl.java
@@ -2,6 +2,7 @@ package com.project.company_projects_management.service;
 
 import com.project.company_projects_management.dao.EmployeeDAO;
 import com.project.company_projects_management.entity.Employee;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,10 +13,23 @@ public class EmployeeServiceImpl implements EmployeeService {
     private EmployeeDAO employeeDAO;
 
     @Autowired
-    public void setEmployeeDAO(EmployeeDAO employeeDAO) {}
+    public void setEmployeeDAO(EmployeeDAO employeeDAO) {
+        this.employeeDAO = employeeDAO;
+    }
 
     @Override
     public List<Employee> getAllEmployees() {
         return employeeDAO.findAll();
+    }
+
+    @Override
+    public Employee getEmployeeById(Long id) {
+        return employeeDAO.findById(id);
+    }
+
+    @Transactional
+    @Override
+    public Employee saveEmployee(Employee employee) {
+        return employeeDAO.save(employee);
     }
 }
