2f9ae89e0cb8e5f7e7c5fc1c5d37bb431398d5eb
mkn <192448446+mika0798@users.noreply.github.com>
2025-12-19 09:25:21 +0700

    Enhance security configs for h2 db and swagger

--- src/main/java/com/project/company_projects_management/config/EmployeeSecurityConfig.java
+++ src/main/java/com/project/company_projects_management/config/EmployeeSecurityConfig.java
@@ -2,15 +2,22 @@ package com.project.company_projects_management.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.RequestMatcher;
+import org.springframework.util.AntPathMatcher;
 
+@Profile("dev")
 @Configuration
 @EnableWebSecurity
 public class EmployeeSecurityConfig {
@@ -41,19 +48,37 @@ public class EmployeeSecurityConfig {
 
         http.authorizeHttpRequests(configurer ->
                         configurer
+                                // ===== H2 console & Swagger =====
+                                .requestMatchers("/docs/**","/swagger-ui/**", "/v3/api-docs/**", "/swagger-ui.html").hasRole("ADMIN")
+                                // ===== Employee APIs =====
                                 .requestMatchers(HttpMethod.GET,"/api/employees").hasRole("EMPLOYEE")
                                 .requestMatchers(HttpMethod.GET,"/api/employees/**").hasRole("EMPLOYEE")
                                 .requestMatchers(HttpMethod.POST,"/api/employees").hasRole("MANAGER")
                                 .requestMatchers(HttpMethod.PUT,"/api/employees/**").hasRole("MANAGER")
                                 .requestMatchers(HttpMethod.PATCH,"/api/employees/**").hasRole("MANAGER")
                                 .requestMatchers(HttpMethod.DELETE,"/api/employees/**").hasRole("ADMIN")
-                                .requestMatchers("/swagger-ui/**", "/v3/api-docs/**", "/swagger-ui.html").hasRole("ADMIN")
+                                // ===== Other requests =====
                                 .anyRequest().authenticated()
-        ).csrf(csrf -> csrf
-                .ignoringRequestMatchers("/api/**","/h2-console/**")
-        ).headers(headers -> headers.frameOptions(frame -> frame.disable())
-        ).httpBasic(Customizer.withDefaults());
+
+        )
+                // ===== CSRF =====
+                .csrf(csrf -> csrf
+//                                .disable()
+                        .ignoringRequestMatchers("/api/**", "/h2-console/**")
+//        )
+//                .httpBasic(httpBasicCustomizer -> httpBasicCustomizer.disable()
+
+        )
+                // ===== Authentication =====
+                .httpBasic(Customizer.withDefaults()
+        )
+                // ===== Allow iframe for H2 console UI =====
+                .headers(headers -> headers
+                        .frameOptions(frame -> frame.disable())
+
+        );
 
         return http.build();
     }
+
 }
