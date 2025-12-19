536333cfe29d8501a077a2826498406e0b517598
mkn <192448446+mika0798@users.noreply.github.com>
2025-12-19 09:23:02 +0700

    Swagger config

---
+++ src/main/java/com/project/company_projects_management/config/SwaggerConfig.java
@@ -0,0 +1,14 @@
+package com.project.company_projects_management.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import org.springframework.context.annotation.Configuration;
+
+//@Configuration
+//@OpenAPIDefinition(info=@Info(title="My API",version="v1"),security=@SecurityRequirement(name="basicAuth"))
+//@SecurityScheme(name="Basic Authentication",type=SecuritySchemeType.HTTP,scheme="basic")
+public class SwaggerConfig {
+}
