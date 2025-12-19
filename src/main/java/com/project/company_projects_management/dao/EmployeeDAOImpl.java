08f24ed7a08939b11dc47e947d34a89aae229a5c
mkn <192448446+mika0798@users.noreply.github.com>
2025-12-19 09:23:31 +0700

    Remove bean declaration

--- src/main/java/com/project/company_projects_management/dao/EmployeeDAOImpl.java
+++ src/main/java/com/project/company_projects_management/dao/EmployeeDAOImpl.java
@@ -3,19 +3,17 @@ package com.project.company_projects_management.dao;
 import com.project.company_projects_management.entity.Employee;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.TypedQuery;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
-@Repository
+
 public class EmployeeDAOImpl implements EmployeeDAO {
 
     private EntityManager entityManager;
 
-    @Autowired
-    EmployeeDAOImpl(EntityManager entityManager) {
+
+    public EmployeeDAOImpl(EntityManager entityManager) {
         this.entityManager = entityManager;
     }
 
