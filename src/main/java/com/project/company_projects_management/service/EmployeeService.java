f47c7b9c16deeb7edb04ff2a4d8f8cd58a50c4df
mkn <192448446+mika0798@users.noreply.github.com>
2025-12-19 09:24:51 +0700

    Add two more methods

--- src/main/java/com/project/company_projects_management/service/EmployeeService.java
+++ src/main/java/com/project/company_projects_management/service/EmployeeService.java
@@ -1,12 +1,16 @@
 package com.project.company_projects_management.service;
 
+import com.project.company_projects_management.dto.EmployeeRequest;
 import com.project.company_projects_management.entity.Employee;
 
 import java.util.List;
+import java.util.Map;
 
 public interface EmployeeService {
     List<Employee> getAllEmployees();
     Employee getEmployeeById(Long id);
     Employee saveEmployee(Employee employee);
     void deleteEmployeeById(Long id);
+    Employee convertEmployee(Long id, EmployeeRequest emRequest);
+    Employee patchEmployee(Map<String,Object> patchPayload, Employee employee);
 }
